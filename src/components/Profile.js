de2e5d5b78dbfed6ee235dc3439281f2d89c6c16
WMBH <itsyourwaytohell@gmail.com>
2020-04-20 19:34:27 +0300

    base structure

---
+++ src/components/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<div>
+				<img src="https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-600w-721703848.jpg" />
+			</div>
+
+			<div>ava + opisanie</div>
+			<div>
+				My posts
+				<div>New posts</div>
+				<div>New posts</div>
+				<div>New posts</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
