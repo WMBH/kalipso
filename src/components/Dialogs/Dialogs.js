fd61fe4b8d31766a826ec3ba59e0e473ef49f082
WMBH <itsyourwaytohell@gmail.com>
2020-04-21 16:46:21 +0300

    dialogs navigation added

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
 	return (
 		<div className={styles.dialogs}>
 			<div className={styles.dialogsItems}>
-				<div className={`${styles.dialog} ${styles.active}`}>Dymich</div>
-				<div className={styles.dialog}>Dymich</div>
-				<div className={styles.dialog}>Dymich</div>
-				<div className={styles.dialog}>Dymich</div>
-				<div className={styles.dialog}>Dymich</div>
+				<div className={`${styles.dialog} ${styles.active}`}>
+					<NavLink to="/dialogs/1">Dymich</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/2">Dymich</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/4">Dymich</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/5">Dymich</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/6">Dymich</NavLink>
+				</div>
 			</div>
 			<div className={styles.messages}>
 				<div className={styles.message}>privet</div>
