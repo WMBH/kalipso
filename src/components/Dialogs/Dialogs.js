9f1d5ca521b824f43dd5216e7b17ac2546b78900
WMBH <itsyourwaytohell@gmail.com>
2020-04-21 16:30:15 +0300

    dialogs page css fixes

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,13 +3,19 @@ import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
 	return (
-		<div className="dialogs">
-			<div className="dialogs-items">
-				<div className="item">Dymich</div>
-				<div className="item">Dymich</div>
-				<div className="item">Dymich</div>
-				<div className="item">Dymich</div>
-				<div className="item">Dymich</div>
+		<div className={styles.dialogs}>
+			<div className={styles.dialogsItems}>
+				<div className={`${styles.dialog} ${styles.active}`}>Dymich</div>
+				<div className={styles.dialog}>Dymich</div>
+				<div className={styles.dialog}>Dymich</div>
+				<div className={styles.dialog}>Dymich</div>
+				<div className={styles.dialog}>Dymich</div>
+			</div>
+			<div className={styles.messages}>
+				<div className={styles.message}>privet</div>
+				<div className={styles.message}>kak dela</div>
+				<div className={styles.message}>kak pogoda</div>
+				<div className={styles.message}>azaza</div>
 			</div>
 		</div>
 	);
