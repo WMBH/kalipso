79023907d85927e6343e335dce39dbd05f703fb9
WMBH <itsyourwaytohell@gmail.com>
2020-05-03 16:13:04 +0300

    major refactoring

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Nav from './components/Nav/Nav';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import './App.css';
-import { Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
-let dialogsData = [
-	{ id: 1, name: 'Dymich' },
-	{ id: 2, name: 'Alena' },
-	{ id: 3, name: 'KOzel' },
-	{ id: 4, name: 'Deda' },
-	{ id: 5, name: 'Dyadya' }
-];
+const App = (props) => {
+    return (
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs'
+                           render={ () => <Dialogs store={props.store} /> }/>
 
-let messagesData = [
-	{ id: 1, message: 'Hi' },
-	{ id: 2, message: 'how are u' },
-	{ id: 3, message: 'how azaza' },
-	{ id: 4, message: 'how azaza' }
-];
-
-let postsData = [ { id: 1, post: 'Hi' }, { id: 2, post: 'HI!' }, { id: 3, post: 'HI!' }, { id: 4, post: 'HI!' } ];
-
-const App = () => {
-	return (
-		<div className="app-wrapper">
-			<Header />
-			<Nav />
-			<div className="app-wrapper-content">
-				<Route
-					path="/dialogs"
-					render={() => <Dialogs dialogsData={dialogsData} messagesData={messagesData} />}
-				/>
-				<Route path="/profile" render={() => <Profile postsData={postsData} />} />
-			</div>
-		</div>
-	);
-};
+                    <Route path='/profile'
+                           render={ () => <Profile
+                               profilePage={props.state.profilePage}
+                               dispatch={props.dispatch} /> }/>
+                </div>
+            </div>
+        )
+}
 
 export default App;
