fa152765140adf6fc06ac82a0a3a33e19871ef3d
WMBH <itsyourwaytohell@gmail.com>
2020-05-06 13:30:37 +0300

    connect provided for Dialogs and Profile

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
-import { StoreContext } from '../../context';
-
-import DialogsView from './DialogsView';
-
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs-reducer';
+import DialogsView from './DialogsView';
+import { connect } from 'react-redux';
 
-const Dialogs = () => {
-	return (
-		<StoreContext.Consumer>
-			{(store) => {
-				let state = store.getState().dialogsPage;
+// const Dialogs = () => {
+// 	return (
+// 		<StoreContext.Consumer>
+// 			{(store) => {
+// 				let state = store.getState().dialogsPage;
 
-				let onSendMessageClick = () => {
-					store.dispatch(sendMessageCreator());
-				};
+// 				let onSendMessageClick = () => {
+// 					store.dispatch(sendMessageCreator());
+// 				};
 
-				let onNewMessageChange = (body) => {
-					store.dispatch(updateNewMessageBodyCreator(body));
-				};
+// 				let onNewMessageChange = (body) => {
+// 					store.dispatch(updateNewMessageBodyCreator(body));
+// 				};
 
-				return (
-					<DialogsView
-						updateNewMessageBodyCreator={onNewMessageChange}
-						sendMessage={onSendMessageClick}
-						dialogsPage={state}
-					/>
-				);
-			}}
-		</StoreContext.Consumer>
-	);
+// 				return (
+// 					<DialogsView
+// 						updateNewMessageBodyCreator={onNewMessageChange}
+// 						sendMessage={onSendMessageClick}
+// 						dialogsPage={state}
+// 					/>
+// 				);
+// 			}}
+// 		</StoreContext.Consumer>
+// 	);
+// };
+
+let mapStateToProps = (state) => {
+	return {
+		dialogsPage: state.dialogsPage
+	};
 };
+
+let mapDispatchToProps = (dispatch) => {
+	return {
+		updateNewMessageBody: (body) => {
+			dispatch(updateNewMessageBodyCreator(body));
+		},
+		sendMessage: () => {
+			dispatch(sendMessageCreator());
+		}
+	};
+};
+const Dialogs = connect(mapStateToProps, mapDispatchToProps)(DialogsView);
 
 export default Dialogs;
