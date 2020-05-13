d10b21b7c5ee25a3acd93c234a797b9cf236af00
WMBH <itsyourwaytohell@gmail.com>
2020-05-13 18:03:15 +0300

    REST API for users provided

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,37 +1,13 @@
 import React from 'react';
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/AvatarPlaceholder.jpg';
 
 let Users = (props) => {
 	if (props.users.length === 0) {
-		props.setUsers([
-			{
-				id: 1,
-				photoUrl:
-					'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-				followed: false,
-				fullName: 'Dmitry',
-				status: 'I am a boss',
-				location: { city: 'Minsk', country: 'Belarus' }
-			},
-			{
-				id: 2,
-				photoUrl:
-					'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-				followed: true,
-				fullName: 'Sasha',
-				status: 'I am a boss too',
-				location: { city: 'Moscow', country: 'Russia' }
-			},
-			{
-				id: 3,
-				photoUrl:
-					'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-				followed: false,
-				fullName: 'Andrew',
-				status: 'I am a boss too',
-				location: { city: 'Kiev', country: 'Ukraine' }
-			}
-		]);
+		axios.get('https://social-network.samuraijs.com/api/1.0/users').then((res) => {
+			props.setUsers(res.data.items);
+		});
 	}
 
 	return (
@@ -40,7 +16,10 @@ let Users = (props) => {
 				<div key={u.id}>
 					<span>
 						<div>
-							<img src={u.photoUrl} className={styles.userPhoto} />
+							<img
+								src={u.photos.small != null ? u.photos.small : userPhoto}
+								className={styles.userPhoto}
+							/>
 						</div>
 						<div>
 							{u.followed ? (
@@ -64,12 +43,12 @@ let Users = (props) => {
 					</span>
 					<span>
 						<span>
-							<div>{u.fullName}</div>
+							<div>{u.name}</div>
 							<div>{u.status}</div>
 						</span>
 						<span>
-							<div>{u.location.country}</div>
-							<div>{u.location.city}</div>
+							<div>{'u.location.country'}</div>
+							<div>{'u.location.city'}</div>
 						</span>
 					</span>
 				</div>
